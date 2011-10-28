c47eee2a7af0df60b6021fbd11adb748f21ef7b7
Danilo Shiga <daniloshiga@gmail.com>
2011-10-28 01:26:57 -0200

    added first draft of what a GET handler looks like

---
+++ api/handlers.py
@@ -0,0 +1,18 @@
+from piston.handler import BaseHandler
+from myapp.models import Pauta
+
+class PautaHandler(BaseHandler):
+    allowed_methods('GET','PUT','POST','DELETE')
+    model = Pauta
+
+    def read(self, request, id=None):
+        """
+        First draft of what GET /api/pautas should return
+        """
+
+        base = Pauta.objects
+
+        if id:
+            return base.get(pk=id)
+        else:
+            return base.all()
