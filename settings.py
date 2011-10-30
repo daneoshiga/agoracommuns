3fd6d442cac0b1c387516549cdae520dcb69f722
Danilo Shiga <daniloshiga@gmail.com>
2011-10-30 12:50:13 -0200

    mysql login info shouldn't be available

--- settings.py
+++ settings.py
@@ -13,9 +13,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'agoracommuns',                      # Or path to database file if using sqlite3.
-        'USER': 'mysqlabmacambira',                      # Not used with sqlite3.
-        'PASSWORD': '#Daivesj',                  # Not used with sqlite3.
-        'HOST': 'mysql.labmacambira.daniloshiga.com',                      # Set to empty string for localhost. Not used with sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
