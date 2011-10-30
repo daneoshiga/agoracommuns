7de41f11883d153405429b0e7a13e9aa5470500d
Danilo Shiga <daniloshiga@gmail.com>
2011-10-30 16:24:20 -0200

    added the other models to admin

--- core/admin.py
+++ core/admin.py
@@ -2,3 +2,7 @@ from core.models import *
 from django.contrib import admin
 
 admin.site.register(Pauta)
+admin.site.register(Usuario)
+admin.site.register(Deliberacao)
+admin.site.register(Voto)
+admin.site.register(Comentario)
