43cac6494573b35d6ae9c832afa7b9785bccc5fb
Danilo Shiga <daniloshiga@gmail.com>
2011-11-09 08:45:50 -0200

    fixed scripts loading

--- tests/clientjquery/js/agoracommuns.js
+++ tests/clientjquery/js/agoracommuns.js
@@ -1,25 +1,36 @@
-Modernizr.load({
-    'jquery'    :   'jquery-1.6.2.min.js',
-    'jqueryui'  :   'jquery-ui-1.8.16.custom.min.js'
-});
+Modernizr.load([{
+    load: {
+        'jquery'    :   'js/jquery-1.6.2.min.js',
+        'jqueryui'  :   'js/jquery-ui-1.8.16.custom.min.js'
+        },
 
-baseUrl = "http://127.0.0.1:8000/";
+    complete: function () {
+        if (correct) {
+            onComplete();
+        }
+    }
+}]);
 
 
-function doAjaxCall(type, url, data, callback) {
-    $.ajax({
-        type: type,
-        url: url,
-        dataType: "json",
-        data: data,
-        success: callback
-    });
-}
+function onComplete() {
+    baseUrl = "http://127.0.0.1:8000/";
 
-function test(anyvar) {
-    console.log(anyvar);
+    $
+    function doAjaxCall(type, url, data, callback) {
+        $.ajax({
+            type: type,
+            url: url,
+            dataType: "json",
+            data: data,
+            success: callback
+        });
     }
 
-$("#pautas").click(function () {
-    doAjaxCall("GET", baseurl+"api/pautas/",data,test);
-});
+    function test(anyvar) {
+        console.log(anyvar);
+    }
+
+    $("#pautas").click(function () {
+        doAjaxCall("GET", baseurl+"api/pautas/",data,test);
+    });
+}
