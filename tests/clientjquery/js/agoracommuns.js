29d4bfdeb50205340e52e5cd8666c94a82cd9b3a
Danilo Shiga <daniloshiga@gmail.com>
2011-11-09 01:53:33 -0200

    leaving a base url parameter to make it easier test it

--- tests/clientjquery/js/agoracommuns.js
+++ tests/clientjquery/js/agoracommuns.js
@@ -3,6 +3,9 @@ Modernizr.load({
     'jqueryui'  :   'jquery-ui-1.8.16.custom.min.js'
 });
 
+baseUrl = "http://127.0.0.1:8000/";
+
+
 function doAjaxCall(type, url, data, callback) {
     $.ajax({
         type: type,
