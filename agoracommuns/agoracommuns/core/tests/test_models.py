1bc34636307e5d14c806a1497de70cb020b4cfb4
Danilo Shiga <daniloshiga@gmail.com>
2014-01-23 22:32:33 -0200

    Adding Deliberation instance creation test

--- agoracommuns/agoracommuns/core/tests/test_models.py
+++ agoracommuns/agoracommuns/core/tests/test_models.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 
 from model_mommy import mommy
 
-from core.models import Agenda
+from core.models import Agenda, Deliberation
 
 
 class CoreModelsTests(TestCase):
@@ -12,3 +12,8 @@ class CoreModelsTests(TestCase):
         agenda = mommy.make(Agenda)
         self.assertTrue(isinstance(agenda, Agenda))
         self.assertEqual(agenda.__unicode__(), agenda.title)
+
+    def test_deliberation_create(self):
+        deliberation = mommy.make(Deliberation)
+        self.assertTrue(isinstance(deliberation, Deliberation))
+        self.assertEqual(deliberation.__unicode__(), deliberation.proposal)
